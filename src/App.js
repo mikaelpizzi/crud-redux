dddb50f6dc7e54a778f05bba36b2a276dbe32862
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-22 11:19:43 -0400

    [ADD] Config Redux and create store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import Header from "./components/Header";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Products from "./components/Products";
 import NewProduct from "./components/NewProduct";
 import EditProduct from "./components/EditProduct";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
     <Router>
-      <Header />
+      <Provider store={store}>
+        <Header />
 
-      <div className="container mt-5">
-        <Routes>
-          <Route exact path="/" element={<Products />} />
-          <Route exact path="/products/new" element={<NewProduct />} />
-          <Route exact path="/products/edit/:id" element={<EditProduct />} />
-        </Routes>
-      </div>
+        <div className="container mt-5">
+          <Routes>
+            <Route exact path="/" element={<Products />} />
+            <Route exact path="/products/new" element={<NewProduct />} />
+            <Route exact path="/products/edit/:id" element={<EditProduct />} />
+          </Routes>
+        </div>
+      </Provider>
     </Router>
   );
 }
