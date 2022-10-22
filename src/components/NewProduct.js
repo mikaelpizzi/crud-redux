da5f17321b3cf9a89ae151c857e2018530061022
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-22 00:15:16 -0400

    [FEAT] Create new product form

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,6 +1,43 @@
 const NewProduct = () => {
     return (
-        <h1>New Product</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Add New Product
+                        </h2>
+
+                        <form>
+                            <div className="form-group">
+                                <label>Product Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Product Name"
+                                    name="name"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Product Price</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Product Price"
+                                    name="price"
+                                />
+                            </div> 
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >Add</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
  
