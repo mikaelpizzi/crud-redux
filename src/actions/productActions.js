7738025bd8662f85e9476c14b901ca101cb494ca
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-22 20:48:31 -0400

    [FEAT] Insert product in API

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,15 +3,22 @@ import {
     ADD_PRODUCT_SUCCESS,
     ADD_PRODUCT_ERROR
 } from "../types";
+import axiosClient from "../config/axios";
 
 // Create new products 
 export function createNewProductAction(product) {
-    return (dispatch) => {
+    return async (dispatch) => {
         dispatch( addProduct() );
 
         try {
+            // Insert in API
+            await axiosClient.post('/prodducts', product);
+
+            // If all went ok, update state
             dispatch( addProductSuccess(product) );
         } catch (error) {
+            console.log(error);
+            // If there's an error, update state
             dispatch( addProductError(true) );
         }
     }
