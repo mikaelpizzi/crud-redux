5b5f70dbf0146cd33f85d00b3eb1f345664d5e4a
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-23 20:48:57 -0400

    [FIX] Fix links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,14 @@ const Header = () => {
     return (  
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
             <div className="container">
-                <h1> <Link to='/' className="text-light">
+                <h1> <Link to='/crud-redux' className="text-light">
                      CRUD - React, Redux, REST API & Axios 
                     </Link> 
                 </h1>
             </div>
 
             <Link 
-                to='/products/new'
+                to='/crud-redux/products/new'
                 className="btn btn-danger nuevo-post d-block d-md-inline-block">
             Add Product &#43;</Link>
         </nav>
