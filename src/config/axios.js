a888194024f37349da4619a309301b16b9926757
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-23 20:30:46 -0400

    [FEAT] Use MyJsonServer as API hosting

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://my-json-server.typicode.com/mikaelpizzi/crud-redux-server/'
 });
 
 export default axiosClient;
